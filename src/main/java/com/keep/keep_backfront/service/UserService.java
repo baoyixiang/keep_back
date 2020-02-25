7d385fd38fe8c4fd2703251cd05166c81b0dd6d9
name <739205298@qq.com>
2020-02-26 00:50:39 +0800

    解冲突

--- src/main/java/com/keep/keep_backfront/service/UserService.java
+++ src/main/java/com/keep/keep_backfront/service/UserService.java
@@ -8,7 +8,6 @@ import com.keep.keep_backfront.VO.inVO.user.UserLoginVO;
 import com.keep.keep_backfront.dao.UserDao;
 import com.keep.keep_backfront.entity.User;
 import com.keep.keep_backfront.entity.UserAttention;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -22,7 +21,8 @@ public class UserService {
 
     private WxLoginHelper wxLoginHelper;
 
-    public UserService(WxLoginHelper wxLoginHelper) {
+    public UserService(UserDao userDao, WxLoginHelper wxLoginHelper) {
+        this.userDao = userDao;
         this.wxLoginHelper = wxLoginHelper;
     }
 
@@ -31,7 +31,8 @@ public class UserService {
      */
     public User login(UserLoginVO userLoginVO){
         User user = new User();
-        WxResponse wxResponse = wxLoginHelper.loginWx(userLoginVO.getCode());//报空指针异常
+        System.out.println("1");
+        WxResponse wxResponse = wxLoginHelper.loginWx(userLoginVO.getCode());
         User curUser = userDao.getUserByWechatId(wxResponse.getOpenId());
         if(curUser == null) {
             try {
@@ -50,35 +51,8 @@ public class UserService {
                 e.printStackTrace();
             }
         }
-        user = userDao.getUserByWechatId(wxResponse.getOpenId());
+        System.out.println("3");
         return user;
-
-//        if(curUser==null){  //登录的用户不存在,则将用户信息插入数据库
-//            try {
-//                int effectedNum = 0;
-//
-//                User user = new User();
-//                user.setWechatId(userLoginVO.getWechatId());
-//                user.setAvatar(userLoginVO.getAvatar());
-//                user.setName(userLoginVO.getName());
-//                user.setPersonalSignature(userLoginVO.getPersonalSignature());
-//                user.setGender(userLoginVO.getGender());
-//                user.setLastLoginTime(new Date());
-//                user.setBeansCount(0);
-//                user.setRecommended(false);
-//                effectedNum = userDao.newUser(user);
-//
-//                if (effectedNum == 1) {
-//                    return ResponseEntity.status(HttpStatus.OK).build();
-//                } else {
-//                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-//                }
-//            } catch (Exception ex) {
-//                ex.printStackTrace();
-//                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-//            }
-//        }
-//        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     /**
